738df59383a48a8c861f813e6324bb52035a82d2
Ryan Ashcraft <ryan@ryanashcraft.me>
2014-02-09 16:58:53 -0800

    Add ground animation. Prevent scrolling. Clean up.

--- js/flappy-bird.js
+++ js/flappy-bird.js
@@ -1,86 +1,151 @@
 "use strict";
 
-YUI().use('node-base', 'gallery-canvas', 'io-base', 'promise', function (Y) {
-  var canvas = Y.one('#game');
-  var context = canvas.invoke('getContext', '2d');
+YUI().use('node-base', 'gallery-canvas', 'io-base', 'promise', 'event', function (Y) {
+  function FlappyBirdGame(canvas, atlasCanvas) {
+    var exports = {};
 
-  var atlasCanvas = Y.one('#atlas');
-  var atlasContext = atlasCanvas.invoke('getContext', '2d');
+    var context = canvas.invoke('getContext', '2d');
+    var atlasContext = atlasCanvas.invoke('getContext', '2d');
+    var sprites;
 
-  function atlas(atlasPath) {
-    return new Y.Promise(function (resolve, reject) {
-      Y.io(atlasPath, {
-        on: {
-          success: function (id, response) {
-            resolve(response.responseText);
-          },
-          failure: function (id, response) {
-            reject(new Error(atlasPath + " request failed: " + response));
-          }
-        }
+    exports.init = function (callback) {
+      canvas.on('touchstart', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
       });
-    });
-  }
 
-  function drawAtlas(imagePath) {
-    return new Y.Promise(function (resolve, reject) {
-      var image = new Image();
+      function getAtlas(atlasPath) {
+        return new Y.Promise(function (resolve, reject) {
+          Y.io(atlasPath, {
+            on: {
+              success: function (id, response) {
+                resolve(response.responseText);
+              },
+              failure: function (id, response) {
+                reject(new Error(atlasPath + " request failed: " + response));
+              }
+            }
+          });
+        });
+      }
 
-      image.onload = function () {
-        atlasContext.drawImage(image, 0, 0);
+      function drawAtlas(imagePath) {
+        return new Y.Promise(function (resolve, reject) {
+          var image = new Image();
 
-        resolve();
-      };
+          image.onload = function () {
+            atlasContext.drawImage(image, 0, 0);
 
-      image.src = imagePath;
-    });
-  }
+            resolve();
+          };
 
-  function Sprite(id, x, y, width, height, context) {
-    this.id = id;
-    this.width = parseInt(width, 10);
-    this.height = parseInt(height, 10);
+          image.src = imagePath;
+        });
+      }
 
-    this.image = context.getImageData(parseFloat(x, 10) * 1024,
-                                      parseFloat(y, 10) * 1024,
-                                      this.width,
-                                      this.height);
-  }
+      context.fillStyle = '#000000';
+      context.fillRect(0, 0, canvas.get('width'), canvas.get('height'));
 
-  Sprite.prototype = {
-    draw: function (context, x, y) {
-      context.putImageData(this.image, x, y);
+      Y.batch(
+        getAtlas('/assets/atlas.txt'),
+        drawAtlas('/assets/atlas.png')
+      ).then(function (data) {
+        var atlas = data[0];
+
+        sprites = atlas.split('\n').reduce(function (previousValue, line) {
+          var tokens = line.split(' ');
+
+          if (tokens.length <= 5) return previousValue;
+
+          var id = tokens[0];
+          var x = tokens[3];
+          var y = tokens[4];
+          var width = tokens[1];
+          var height = tokens[2];
+
+          previousValue[id] = new Sprite(id, x, y, width, height, atlasContext);
+
+          return previousValue;
+        }, {});
+
+        callback();
+      });
+    };
+
+    exports.tick = function (timestamp) {
+      context.clearRect(0, 0, canvas.get('width'), canvas.get('height'));
+
+      sprites['bg_day'].draw(context);
+      sprites['bg_day'].setX(sprites['bg_day'].getWidth() - 10);
+      sprites['bg_day'].draw(context);
+      sprites['bg_day'].setX(0);
+
+      sprites['land'].setY(canvas.get('height') - sprites['land'].getHeight());
+      sprites['land'].setX(sprites['land'].getX() - 1);
+      sprites['land'].draw(context);
+      sprites['land'].setX(sprites['land'].getX() + sprites['land'].getWidth());
+      sprites['land'].draw(context);
+      sprites['land'].setX(sprites['land'].getX() - sprites['land'].getWidth());
+      if (sprites['land'].getX() <= -sprites['land'].getWidth()) {
+        sprites['land'].setX(0);
+      }
+
+      requestAnimationFrame(exports.tick);
     }
+
+    return exports;
   }
 
-  context.fillStyle = '#000000';
-  context.fillRect(0, 0, canvas.get('width'), canvas.get('height'));
+  function Sprite(id, atlasX, atlasY, atlasWidth, atlasHeight, atlasContext) {
+    var exports = {};
 
-  Y.batch(
-    atlas('/assets/atlas.txt'),
-    drawAtlas('/assets/atlas.png')
-    ).then(function (data) {
-      var atlas = data[0];
+    var id = id;
+    var x = 0;
+    var y = 0;
+    var width = parseInt(atlasWidth, 10);
+    var height = parseInt(atlasHeight, 10);
 
-      var sprites = atlas.split('\n').reduce(function (previousValue, line) {
-        var tokens = line.split(' ');
+    var image = atlasContext.getImageData(parseFloat(atlasX, 10) * 1024,
+                                           parseFloat(atlasY, 10) * 1024,
+                                           atlasWidth,
+                                           atlasHeight);
 
-        if (tokens.length <= 5) return previousValue;
+    exports.draw = function(context) {
+      context.putImageData(image, x, y);
+    }
 
-        var id = tokens[0];
-        var x = tokens[3];
-        var y = tokens[4];
-        var width = tokens[1];
-        var height = tokens[2];
+    exports.setX = function(newValue) {
+      x = newValue;
+    }
 
-        previousValue[id] = new Sprite(id, x, y, width, height, atlasContext);
+    exports.setY = function(newValue) {
+      y = newValue;
+    }
 
-        return previousValue;
-      }, []);
+    exports.getX = function() {
+      return x;
+    }
 
-      sprites['bg_day'].draw(context, 0, 0);
-      sprites['bg_day'].draw(context, sprites['bg_day'].width, 0);
+    exports.getY = function() {
+      return y;
+    }
 
-      sprites['land'].draw(context, 0, canvas.get('height') - sprites['land'].height);
-    });
+    exports.getWidth = function() {
+      return width;
+    }
+
+    exports.getHeight = function() {
+      return height;
+    }
+
+    return exports;
+  }
+
+  var canvas = Y.one('#game');
+  var atlasCanvas = Y.one('#atlas');
+  var game = new FlappyBirdGame(canvas, atlasCanvas);
+
+  game.init(function () {
+    requestAnimationFrame(game.tick);
+  });
 });
